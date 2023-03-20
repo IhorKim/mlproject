a64f7b40ef42bf766e7b80d829685253e78a6f91
IhorKim <kimiu234786@gmail.com>
2023-03-20 14:01:46 -0600

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-import logging
-import logger
+from src.logger import logging
 
 
 def error_message_details(error, error_detail: sys):
@@ -16,7 +15,7 @@ class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
         self.error_message = error_message_details(error_message, error_detail=error_detail)
-        logging.error(self.error_message)
+
 
     def __str__(self):
         return self.error_message
